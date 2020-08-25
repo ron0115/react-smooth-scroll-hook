628fb4264be1d210a56d15041c57798d090912d1
Ron0115 <jayguess@qq.com>
2020-08-25 16:17:23 +0800

    chore: example init

--- example/index.tsx
+++ example/index.tsx
@@ -1,9 +1,36 @@
-import 'react-app-polyfill/ie11';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import "react-app-polyfill/ie11";
+import * as ReactDOM from "react-dom";
+import React, { useRef } from "react";
+import { useSmoothScroll } from "react-smooth-scroll-hook";
+export default function App() {
+  const ref = useRef(null);
+  const { scrollTo } = useSmoothScroll({
+    ref
+  });
+  return (
+    <div className="App">
+      <h1>react-smooth-scroll-hook</h1>
+      <button onClick={() => scrollTo("#item-10")}>scrollTo("#item-10")</button>
+      <button onClick={() => scrollTo(Infinity)}>scrollTo(Infinity)</button>
+      <button onClick={() => scrollTo(-Infinity)}>scrollTo(-Infinity)</button>
+      <br />
+      <div
+        ref={ref}
+        style={{
+          overflowY: "scroll",
+          maxHeight: "400px"
+        }}
+      >
+        {Array(100)
+          .fill(null)
+          .map((_item, i) => (
+            <div key={i} id={`item-${i}`}>
+              {i}
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+}
 
-const App = () => {
-  return <div></div>;
-};
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
