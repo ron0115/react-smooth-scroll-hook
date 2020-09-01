bf4d092529174e29f3580653871b7c40598eb912
Ron0115 <jayguess@qq.com>
2020-09-01 11:51:46 +0800

    update codesandbox

--- example/index.tsx
+++ example/index.tsx
@@ -1,17 +1,21 @@
-import * as ReactDOM from 'react-dom';
-import * as React from 'react';
-import { Demo } from './Demo.stories';
-import { DirectionX } from './DirectionX.stories';
+import * as ReactDOM from "react-dom";
+import * as React from "react";
+import { Demo } from "./Demo.stories";
+import { DirectionX } from "./DirectionX.stories";
+// import { UseScrollWatch } from "./UseScrollWatch.stories";
+import { Body } from "./Body.stories";
 export default function App() {
   return (
     <div className="App">
-      <h1>react-smooth-scroll-hook</h1>
-      <h2>Demo.tsx</h2>
+      <h1>useSmoothScroll</h1>
+      <h2>Demo.stories.tsx</h2>
       <Demo />
-      <h2>DirectionX.tsx</h2>
+      <h2>Direction.stories.tsx</h2>
       <DirectionX />
+      <h2>Body.stories.tsx</h2>
+      <Body />
     </div>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
